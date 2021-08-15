638ff8ca79bae2265eb2e247226ac35c45d66c92
Muhammad Nurhafiz <nurhafiz2000@gmail.com>
2021-08-15 15:54:21 +0800

    use relative path

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import os
+import sys
 import platform
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.dirname(__file__)
+    return os.path.join(base_path, relative_path)
 
 # Select chromedriver based on os
 # Chromedriver version is 92.*
@@ -16,7 +24,7 @@ elif os_use == "Linux":
 
 try:
 	op = webdriver.ChromeOptions()
-	driver = webdriver.Chrome(executable_path=driver_path,options=op)
+	driver = webdriver.Chrome(executable_path=resource_path(driver_path),options=op)
 
 	driver.get('https://popcat.click')
 	element = driver.find_element_by_xpath('//*[@id="app"]/img')
